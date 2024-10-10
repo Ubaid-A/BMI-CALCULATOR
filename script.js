7bf2209a737b02d1d44ecc78bf614e6a5ac180bb
Ubaid-A <wafaubaid3@gmail.com>
2024-10-10 08:03:13 +0500

    add error message when height and weight is empty

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function bmi() {
     const h = document.getElementById('height').value;
     const w = document.getElementById('weight').value;
-    const bmi = w / (h / 100 * h / 100);
-    const total = bmi.toFixed(2);
-    document.getElementById('result').innerHTML = `Your BMI is ${total}`
 
+    if(h== '' || w == ''){
+        document.getElementById('result').innerHTML = ``;
+        document.getElementById('error').innerHTML = `Please Enter height and weight`
+    }else {
+        document.getElementById('error').innerHTML = ``;
+        const bmi = w / (h / 100 * h / 100);
+        const total = bmi.toFixed(2);
+        document.getElementById('result').innerHTML = `Your BMI is ${total}`
+    }
 }
