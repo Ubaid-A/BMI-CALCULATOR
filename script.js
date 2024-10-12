d82fe5f750f6bf869751c3e6976806e6a2cedc24
Ubaid-A <wafaubaid3@gmail.com>
2024-10-12 10:14:23 +0500

    ft convert meter using js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function bmi() {
     const h = document.getElementById('height').value;
     const w = document.getElementById('weight').value;
+    const meter = Number(h) * 0.3048;
 
     if(h== '' || w == ''){
         document.getElementById('result').innerHTML = ``;
         document.getElementById('error').innerHTML = `Please Enter height and weight`
     }else {
         document.getElementById('error').innerHTML = ``;
-        const bmi = w / (h / 100 * h / 100);
-        const total = bmi.toFixed(2);
-        document.getElementById('result').innerHTML = `Your BMI is ${total}`
+        const bmi = (Number(w) / (meter * meter)).toFixed(2);
+        // const total = bmi.toFixed(2);
+        document.getElementById('result').innerHTML = `Your BMI is ${bmi}`
     }
 }
+
